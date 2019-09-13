3b8df2b024d82b7da827ad67b09042162fd9a85d
Yasin Yaqoobi <yasinyaqoobi@gmail.com>
2019-09-12 21:08:31 -0400

    add eneficient grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import './App.css';
 import FileUpload from './components/FileUpload';
+import GridOne from './GridOne';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    max-width: 1440px;
+    margin: 0 auto;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>A simple file upload</h1>
-      </header>
-      <div style={{maxWidth: 900, margin: '0 auto', marginTop: '2rem'}}>
-        <FileUpload server="http://localhost:4000/api"/>
-      </div>
-    </div>
-  );
+    return (
+        <Container className="App">
+            <header>
+                <h1>A simple file upload</h1>
+            </header>
+            <div style={{ maxWidth: 900, margin: '0 auto', marginTop: '2rem' }}>
+                <FileUpload server="http://localhost:4000/api" />
+            </div>
+            <GridOne />
+        </Container>
+    );
 }
 
 export default App;
---
+++ src/GridOne.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const Container = styled.div`
+    display: flex;
+    margin-top: 2rem;
+    flex-wrap: wrap;
+    margin-left: -0.5rem;
+    margin-right: -0.5rem;
+    justify-content: center;
+`;
+
+const ImageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 24%;
+    box-sizing: border-box;
+    background-color: #fff;
+    -webkit-border-radius: 2px;
+    border-radius: 4px;
+    -webkit-box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+        0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+        0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+    margin: 7px;
+    overflow: hidden;
+    position: relative;
+    -webkit-transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1);
+    transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1);
+    img {
+        width: 100%;
+        flex: 1;
+    }
+`;
+
+const GridOne = () => {
+    const [images, setImage] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/images').then(result => {
+            console.log('res', result);
+            setImage(result.data);
+        });
+    }, []);
+
+    return (
+        <Container>
+            {images.map(image => (
+                <ImageContainer key={image.src}>
+                    <img src={image.src} alt="" />
+                    <FileName src={image.src} />
+                </ImageContainer>
+            ))}
+        </Container>
+    );
+};
+
+const FileName = ({ src }) => {
+    const name = src.substring(src.lastIndexOf('/') + 1, src.indexOf('.'));
+    return <h3>{name}</h3>;
+};
+
+export default GridOne;
